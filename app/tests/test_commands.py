a4623092ccff0767b205cf8a6c1647122578c0aa
jvondie <jason@archemedx.com>
2022-06-13 11:58:55 -0400

    add wait for db

---
+++ app/tests/test_commands.py
@@ -0,0 +1,44 @@
+"""
+Test custom Django management commands
+"""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test Commands"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready"""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for db when getting OperationalError"""
+
+        """
+        The side effect is mocking what we want to test happening.
+        In this case, we are making the side effect return one
+        error twice and another error 3 times.
+        This is the result of some trial and error, when PSQL starts
+        it might return a psycog error
+        before then giving an operational error before coming online.
+        """
+
+        patched_check.side_effect = [
+            Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
